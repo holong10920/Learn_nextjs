8bcd4c16d6b7b6fbd2f8b2b7fb78057a4fb74471
holong10920 <ho.long@marketenterprise.vn>
2023-08-25 14:55:42 +0700

    add service worker firebase

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,21 +7,20 @@ export default function Document() {
 			<Head>
 				<Script strategy="afterInteractive" id="show-banner" type="module">
 					{`
-            import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-            import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
+            importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+            importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
             const firebaseConfig = {
-              apiKey: "AIzaSyAF9gUXamrzAo5yAQ8TTOt5j-OVXGeKsU0",
-              authDomain: "learn-nextjs-375b2.firebaseapp.com",
-              projectId: "learn-nextjs-375b2",
-              storageBucket: "learn-nextjs-375b2.appspot.com",
-              messagingSenderId: "246783941907",
-              appId: "1:246783941907:web:cc56ffae4c9d381019cee6",
-              measurementId: "G-JY9NN5F8YY"
+              apiKey: "AIzaSyDld-b90iV0aRO3-DdsVimNPVwAjsHqayE",
+              authDomain: "laravel-engpush.firebaseapp.com",
+              projectId: "laravel-engpush",
+              storageBucket: "laravel-engpush.appspot.com",
+              messagingSenderId: "1083444428026",
+              appId: "1:1083444428026:web:4eb4431f2630086f281d43"
             };
 
-            const app = initializeApp(firebaseConfig);
-            const analytics = getAnalytics(app);
+            firebase.initializeApp(firebaseConfig);
+            const messaging = firebase.messaging();
           `}
 				</Script>
 			</Head>
